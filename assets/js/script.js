33de7fff005c2792594b5851283b007fe8ae71c9
FrederikWagnerC <frederikwagnercs@gmail.com>
2024-08-26 10:44:09 +0200

    function bygStatics
    tilføjede bygStatics

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,30 @@
 // ONLOAD 
 addEventListener('load', (e) => {
-    hentData()
+    let testStorage = localStorage.getItem('test')
+    console.log(testStorage)
+    // hentData()
     bygStatics()
+    localStorage.setItem('test', 'asd')
 })
 
 
 // FUNCTION HENTER DATA FRA LOCALSTORAGE OG KONVERTERE DEN. 
     // MANGLER KONVERTERING
 
-function hentData(){
-    localStorage.getItem() // MANGLER DATA
+function hentData() {
+    // localStorage.getItem() // MANGLER DATA
     // MANGLER KONVERTERING
-    if(error) {
-        dataError()
-    } else {
-        dataReceived()
-    }
+    // if(error) {
+    //     dataError()
+    // } else {
+    //     dataReceived()
+    // }
+}
+
+// STATICS 
+
+function bygStatics() {
+    bygTitle()
+    bygFooter()
 }
 
