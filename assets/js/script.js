05de9a61a525113d7341bcbd32e2809643c1097f
FrederikWagnerC <frederikwagnercs@gmail.com>
2024-08-26 10:55:01 +0200

    bygTitle
    Lavede functionen bygTitle

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+const body = document.getElementById('app')
+
 // ONLOAD 
 addEventListener('load', (e) => {
     let testStorage = localStorage.getItem('test')
@@ -23,8 +25,19 @@ function hentData() {
 
 // STATICS 
 
+function bygTitle() {
+    const bygHeader = document.createElement('header')
+    bygHeader.setAttribute('id', 'header')
+    body.appendChild(bygHeader)
+
+    const bygH1 = document.createElement('h1')
+    bygH1.setAttribute('id', 'title')
+    bygH1.append('ToDoApp')
+    bygHeader.appendChild(bygH1)
+}
+
 function bygStatics() {
     bygTitle()
-    bygFooter()
+    // bygFooter()
 }
 
