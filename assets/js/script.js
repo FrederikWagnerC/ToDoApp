241338b55bddb37f2bb4a30b24d6b617f381c75a
FrederikWagnerC <frederikwagnercs@gmail.com>
2024-08-26 10:24:41 +0200

    function hentData
    Lavede hvad jeg kunne med hentData og startede de forskellige filer den skal bruges.

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+
+// FUNCTION HENTER DATA FRA LOCALSTORAGE OG KONVERTERE DEN. 
+    // MANGLER KONVERTERING
+
+function hentData(){
+    localStorage.getItem() // MANGLER DATA
+    // MANGLER KONVERTERING
+    if(error) {
+        dataError()
+    } else {
+        dataReceived()
+    }
+}
